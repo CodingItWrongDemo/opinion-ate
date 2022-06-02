d31c56f96402ebc05f05311960c004d233f0ed54
Josh Justice <jjustice@justworks.com>
2022-06-01 20:05:16 -0400

    Style screen to card

--- src/components/RestaurantScreen/index.js
+++ src/components/RestaurantScreen/index.js
@@ -1,4 +1,7 @@
 import {useCallback, useState} from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 import RestaurantList from '../RestaurantList';
 import api from '../../api';
 import loadRestaurants from './loadRestaurants';
@@ -11,12 +14,14 @@ export default function RestaurantScreen() {
   }, []);
 
   return (
-    <div>
-      <h1>Restaurants</h1>
-      <RestaurantList
-        restaurants={restaurants}
-        loadRestaurants={handleLoadRestaurants}
-      />
-    </div>
+    <Card>
+      <CardContent>
+        <Typography variant="h5">Restaurants</Typography>
+        <RestaurantList
+          restaurants={restaurants}
+          loadRestaurants={handleLoadRestaurants}
+        />
+      </CardContent>
+    </Card>
   );
 }
