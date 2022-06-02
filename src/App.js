359e0475bffb717800863f011a12ea92f3d0f85e
Josh Justice <jjustice@justworks.com>
2022-06-01 20:04:32 -0400

    Style app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import {createTheme} from '@mui/material/styles';
+import {green} from '@mui/material/colors';
+import {ThemeProvider} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 import RestaurantScreen from './components/RestaurantScreen';
+
+const theme = createTheme({
+  palette: {
+    primary: green,
+  },
+});
 
 export default function App() {
   return (
-    <div>
-      <RestaurantScreen />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Opinion Ate</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <RestaurantScreen />
+      </Container>
+    </ThemeProvider>
   );
 }
