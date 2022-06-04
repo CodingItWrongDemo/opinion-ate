7150baf33cd78e9321b8bace4c787c021bc061ea
Josh Justice <jjustice@justworks.com>
2022-06-04 10:23:54 -0400

    Group data function tests

--- src/components/RestaurantScreen/loadRestaurants.spec.js
+++ src/components/RestaurantScreen/loadRestaurants.spec.js
@@ -1,20 +1,22 @@
 import loadRestaurants from './loadRestaurants';
 
 describe('loadRestaurants', () => {
-  it('stores the restaurants', async () => {
-    const records = [
-      {id: 1, name: 'Sushi Place'},
-      {id: 2, name: 'Pizza Place'},
-    ];
+  describe('when loading succeeds', () => {
+    it('stores the restaurants', async () => {
+      const records = [
+        {id: 1, name: 'Sushi Place'},
+        {id: 2, name: 'Pizza Place'},
+      ];
 
-    const api = {
-      loadRestaurants: () => Promise.resolve(records),
-    };
+      const api = {
+        loadRestaurants: () => Promise.resolve(records),
+      };
 
-    const setRestaurants = jest.fn().mockName('setRestaurants');
+      const setRestaurants = jest.fn().mockName('setRestaurants');
 
-    await loadRestaurants({api, setRestaurants});
+      await loadRestaurants({api, setRestaurants});
 
-    expect(setRestaurants).toHaveBeenCalledWith(records);
+      expect(setRestaurants).toHaveBeenCalledWith(records);
+    });
   });
 });
