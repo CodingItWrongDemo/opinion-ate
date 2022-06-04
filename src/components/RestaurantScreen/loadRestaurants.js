36e32ba2c53afcd15af368e7d60e03b672a6c27d
Josh Justice <jjustice@justworks.com>
2022-06-04 10:25:53 -0400

    Pass new setLoading test

--- src/components/RestaurantScreen/loadRestaurants.js
+++ src/components/RestaurantScreen/loadRestaurants.js
@@ -1,4 +1,9 @@
-export default async function loadRestaurants({api, setRestaurants}) {
+export default async function loadRestaurants({
+  api,
+  setRestaurants,
+  setLoading,
+}) {
+  setLoading(true);
   const records = await api.loadRestaurants();
   setRestaurants(records);
 }
