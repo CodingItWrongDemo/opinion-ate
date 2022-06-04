960d1a0034a8bea096f63957a71a87be8ba65429
Josh Justice <jjustice@justworks.com>
2022-06-04 11:23:34 -0400

    Specify adding restaurant to list

--- src/components/RestaurantScreen/createRestaurant.spec.js
+++ src/components/RestaurantScreen/createRestaurant.spec.js
@@ -2,6 +2,9 @@ import createRestaurant from './createRestaurant';
 
 describe('createRestaurant', () => {
   const newRestaurantName = 'Sushi Place';
+  const existingRestaurant = {id: 1, name: 'Pizza Place'};
+  const responseRestaurant = {id: 2, name: newRestaurantName};
+  const restaurants = [existingRestaurant];
 
   let api;
 
@@ -14,5 +17,29 @@ describe('createRestaurant', () => {
   it('saves the restaurant to the server', () => {
     createRestaurant({api, name: newRestaurantName});
     expect(api.createRestaurant).toHaveBeenCalledWith(newRestaurantName);
+  });
+
+  describe('when save succeeds', () => {
+    let setRestaurants;
+
+    beforeEach(() => {
+      api.createRestaurant.mockResolvedValue(responseRestaurant);
+
+      setRestaurants = jest.fn();
+
+      return createRestaurant({
+        api,
+        name: newRestaurantName,
+        restaurants,
+        setRestaurants,
+      });
+    });
+
+    it('adds the returned restaurant to the list', () => {
+      expect(setRestaurants).toHaveBeenCalledWith([
+        existingRestaurant,
+        responseRestaurant,
+      ]);
+    });
   });
 });
