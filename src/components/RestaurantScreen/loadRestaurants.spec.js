f38cb7ab1edf0dc9f70c5e0bb16ae81c2f64afc8
Josh Justice <jjustice@justworks.com>
2022-06-04 10:25:18 -0400

    Test loading flag for loadRestaurants

--- src/components/RestaurantScreen/loadRestaurants.spec.js
+++ src/components/RestaurantScreen/loadRestaurants.spec.js
@@ -1,6 +1,20 @@
 import loadRestaurants from './loadRestaurants';
 
 describe('loadRestaurants', () => {
+  describe('while loading', () => {
+    it('sets a loading flag', () => {
+      const api = {
+        loadRestaurants: () => new Promise(() => {}),
+      };
+
+      const setLoading = jest.fn().mockName('setLoading');
+
+      loadRestaurants({api, setLoading});
+
+      expect(setLoading).toHaveBeenCalledWith(true);
+    });
+  });
+
   describe('when loading succeeds', () => {
     it('stores the restaurants', async () => {
       const records = [
