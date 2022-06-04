69cef5206b5a6f081461ae38cbbdc9938849dc2d
Josh Justice <jjustice@justworks.com>
2022-06-04 10:17:15 -0400

    Implement API

--- src/api.js
+++ src/api.js
@@ -1,5 +1,14 @@
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: 'https://api.outsidein.dev/TOPDDhuzdfNDL42ZwUxFfA4C0nGEUaVl',
+});
+
 const api = {
-  loadRestaurants() {},
+  async loadRestaurants() {
+    const response = await client.get('/restaurants');
+    return response.data;
+  },
 };
 
 export default api;
