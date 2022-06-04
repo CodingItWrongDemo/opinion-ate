6f68b67f5f3f9730fe21c9a34d2e677314544f5d
Josh Justice <jjustice@justworks.com>
2022-06-04 11:08:29 -0400

    Add scaffold of new functions

--- src/components/RestaurantScreen/index.js
+++ src/components/RestaurantScreen/index.js
@@ -1,10 +1,26 @@
+import {useState} from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import RestaurantList from '../RestaurantList';
 import NewRestaurantForm from '../NewRestaurantForm';
+import api from '../../api';
+import loadRestaurants from './loadRestaurants';
+import createRestaurant from './createRestaurant';
 
 export default function RestaurantScreen() {
+  const [restaurants, setRestaurants] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadError, setLoadError] = useState(false);
+
+  function handleLoadRestaurants() {
+    loadRestaurants({api, setRestaurants, setLoading, setLoadError});
+  }
+
+  function handleCreateRestaurant(name) {
+    createRestaurant({api, name, restaurants, setRestaurants});
+  }
+
   return (
     <Card>
       <CardContent>
