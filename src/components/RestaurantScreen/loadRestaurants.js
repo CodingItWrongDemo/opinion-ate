0e5e7aa3966d3714e7b785ba2b02bece321699f8
Josh Justice <jjustice@justworks.com>
2022-06-04 10:35:28 -0400

    Set error flag

--- src/components/RestaurantScreen/loadRestaurants.js
+++ src/components/RestaurantScreen/loadRestaurants.js
@@ -2,11 +2,14 @@ export default async function loadRestaurants({
   api,
   setRestaurants,
   setLoading,
+  setLoadError,
 }) {
   try {
     setLoading(true);
     const records = await api.loadRestaurants();
     setRestaurants(records);
     setLoading(false);
-  } catch {}
+  } catch {
+    setLoadError(true);
+  }
 }
