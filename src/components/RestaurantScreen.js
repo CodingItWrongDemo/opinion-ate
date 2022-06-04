6ae37896b24af9bdfecd276ac50a8684ce09c534
Josh Justice <jjustice@justworks.com>
2022-06-04 09:47:03 -0400

    Fix Cypress errors

--- src/components/RestaurantScreen.js
+++ src/components/RestaurantScreen.js
@@ -1,10 +1,18 @@
+import {useState} from 'react';
 import RestaurantList from './RestaurantList';
 
 export default function RestaurantScreen() {
+  const [restaurants, setRestaurants] = useState([]);
+
+  function handleLoadRestaurants() {}
+
   return (
     <div>
       <h1>Restaurants</h1>
-      <RestaurantList />
+      <RestaurantList
+        restaurants={restaurants}
+        loadRestaurants={handleLoadRestaurants}
+      />
     </div>
   );
 }
